82d51acbe04cdde230fd95ada0f357fd0eba0935
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2020-10-19 22:33:37 -0400

    Add project files.

---
+++ PrioAdmin/Models/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrioAdmin.Models
+{
+	public class NewUser
+	{
+		[Required]
+		public string name;
+
+		[Required]
+		public string password;
+
+
+	}
+}
