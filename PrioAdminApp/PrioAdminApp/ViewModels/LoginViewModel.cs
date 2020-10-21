ba20a60557fed23cb26e17ab26bbcfa423d84727
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2020-10-21 17:20:15 -0400

    Restructure

---
+++ PrioAdminApp/PrioAdminApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PrioAdmin.ViewModels
+{
+	class LoginViewModel
+	{
+		// api services
+
+		
+
+		public string username { get; set; }
+
+		public string password { get; set; }
+
+		public ICommand LoginCommand
+		{
+
+			get
+			{
+				return new Command(async() =>
+				{
+					// await _apiServices.LoginAsync(username, password);
+				});
+			}
+		}
+	}
+}
